8f83710e3fb6a370ec971e0241331de3479a66ff
hua1018123 <1678861279@qq.com>
2026-03-26 15:59:21 +0800

    Merge branch 'main' of https://github.com/zyx940805/career-plan # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/career/plan/CareerPlanApplication.java
+++ src/main/java/com/career/plan/CareerPlanApplication.java
@@ -8,7 +8,7 @@ public class CareerPlanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CareerPlanApplication.class, args);
-        System.out.println("Hello");
+        System.out.println("1");
 
     }
 
