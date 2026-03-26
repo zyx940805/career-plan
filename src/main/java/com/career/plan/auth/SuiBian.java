abb49435be50410448d9f757e1ad24e371be0d8b
hua1018123 <1678861279@qq.com>
2026-03-26 17:08:43 +0800

    Merge branch 'main' of https://github.com/zyx940805/career-plan

---
+++ src/main/java/com/career/plan/auth/SuiBian.java
@@ -0,0 +1,10 @@
+package com.career.plan.auth;
+
+public class SuiBian {
+    public SuiBian(){
+        System.out.println("SuiBian");
+    }
+    public static void main(String[] args) {
+        new SuiBian();
+    }
+}
