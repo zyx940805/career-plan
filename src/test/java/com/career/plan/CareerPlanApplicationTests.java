e66f606beb11e728b50a498c386bdbb7d13d9853
zyx940805 <annycandy27@gmail.com>
2026-03-26 15:02:26 +0800

    Initialize project structure

---
+++ src/test/java/com/career/plan/CareerPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.career.plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
